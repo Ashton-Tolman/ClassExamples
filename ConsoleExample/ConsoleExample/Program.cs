1bf754949bad94d380a4ec619a65a3dca009e1c7
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-01-16 09:02:48 -0700

    readd tracked files

---
+++ ConsoleExample/ConsoleExample/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Ashton Tolman
+   Spring 2026
+   RCET 2265
+   Console Example
+   https://github.com/Ashton-Tolman/ClassExamples.git*/
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
